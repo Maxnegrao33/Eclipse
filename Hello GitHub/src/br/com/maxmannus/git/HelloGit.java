70b0e951615b685d1b4ec0a922f6b535fa596e2b
Maciel Ribeiro de Oliveira <38005099+Maxnegrao33@users.noreply.github.com>
2019-12-13 17:20:08 -0300

    Adicionando arquivos via upload
    Aula com o Prof. José de Assis para aprender trabalhar como Git e a IDE Eclipse

---
+++ Hello GitHub/src/br/com/maxmannus/git/HelloGit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.maxmannus.git;
+
+/**
+ * @author Maciel Oliveira
+ *
+ *Exemplo de integração do Eclipse com o GitHub
+ */
+public class HelloGit {
+
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");	
+
+	}
+
+	
+}
