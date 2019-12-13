ff47979269810d99c2cc183da232d173bfd0df45
Maciel Oliveira <Maciel Oliveira@MAXCORPORATION>
2019-12-13 17:43:45 -0300

    Alterações para aprender subir arquivos

--- Hello GitHub/src/br/com/maxmannus/git/HelloGit.java
+++ Hello GitHub/src/br/com/maxmannus/git/HelloGit.java
@@ -13,7 +13,9 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git");	
-
+		System.out.println("Modificação em uma linha para subir arquivos");
+		System.out.println("Precisa para de bugar");
+		
 	}
 
 	
